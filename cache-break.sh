5a5df825568998d9d960ba7d6c67d04a6ad1588b
Frank Flitton <fflitton@gmail.com>
2020-12-12 15:31:39 -0500

    JSON vs JS solution, Diff Quotes

--- cache-break.sh
+++ cache-break.sh
@@ -2,8 +2,30 @@
 
 # Break the cache so new files are loaded by the app.
 BUILD_HASH=$((1 + $RANDOM % 100000))
-TARGET_FILE=".js"
-HASHED_FILE=($TARGET_FILE"?"$BUILD_HASH\")
-echo $HASHED_FILE
-# awk '{gsub(".js", ".js?${BUILD_HASH}", $0); print}' test.html
-# cat test.html | awk -v srch="$TARGET_FILE" -v repl="$HASHED_FILE" '{ sub(srch,repl,$0); print $0 }' >tmp && mv tmp test.html
+HASHED_FILE=""
+
+fileExtHash() {
+  SEPERATOR=$(echo $1 | tail -c 2)
+  F_EXT=$(echo $1 | cut -d"$SEPERATOR" -f 1)
+  HASHED_EXT=$F_EXT"?"$BUILD_HASH$SEPERATOR
+  HASHED_FILE=$HASHED_EXT
+}
+
+replacePath() {
+  INPUT_FILE=$1
+  TARGET_EXT=$2
+  TARGET_HASHED_EXT=$3
+  cat $INPUT_FILE | awk -v srch="$TARGET_EXT" -v repl="$TARGET_HASHED_EXT" '{ sub(srch,repl,$0); print $0 }' >tmp && mv tmp $INPUT_FILE
+  echo "success" INPUT_FILE
+}
+
+HTML_EXTS=("js" "json")
+
+for EXT_ROOT in ${HTML_EXTS[@]}; do
+  # echo $EXT
+  EXT_LIST=("."$EXT_ROOT\" "."$EXT_ROOT\')
+  for EXT in ${EXT_LIST[@]}; do
+    fileExtHash $EXT
+    replacePath test.html $EXT $HASHED_FILE
+  done
+done
