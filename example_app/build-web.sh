232898c464f931c5feeb2e12412b721099deb5cd
Frank Flitton <fflitton@gmail.com>
2020-12-12 02:04:06 -0500

    Add changes to example instance

--- example_app/build-web.sh
+++ example_app/build-web.sh
@@ -1,7 +1,67 @@
 #!/bin/bash
 
-# Build pipeline for Flutter Web
-# Place this file next to your pubspec.yaml file
+##################################################
+#                                                #
+# Flutter For Web Build Pipeline                 #
+#                                                #
+# Place this file next to your pubspec.yaml file #
+#                                                #
+##################################################
+
+# Reminder to check for updates to the build script.
+# Flutter for web is still in beta so expect updates!
+
+CURRENT_VERSION=1.0.1
+REMOTE_URL="https://github.com/FrankFlitton/Flutter-for-web-deploy-script"
+REMOTE_TAGS="$(git ls-remote --tags $REMOTE_URL)"
+
+REMOTE_HASHES=()
+REMOTE_VERSIONS=()
+
+REMOTE_REF_COUNTER=0
+for GIT_META in $REMOTE_TAGS; do
+  if (($REMOTE_REF_COUNTER % 2)); then
+    REMOTE_HASHES+=(
+      $(echo $GIT_META | cut -d "/" -f 3)
+    )
+  fi
+  REMOTE_REF_COUNTER=$(($COUNTER + 1))
+done
+
+REMOTE_VERSION=${REMOTE_HASHES[((${#REMOTE_HASHES[@]} - 1))]}
+
+vercomp() {
+  if [[ $1 == $2 ]]; then
+    return 0
+  fi
+  local IFS=.
+  local i ver1=($1) ver2=($2)
+  # fill empty fields in ver1 with zeros
+  for ((i = ${#ver1[@]}; i < ${#ver2[@]}; i++)); do
+    ver1[i]=0
+  done
+  for ((i = 0; i < ${#ver1[@]}; i++)); do
+    if [[ -z ${ver2[i]} ]]; then
+      # fill empty fields in ver2 with zeros
+      ver2[i]=0
+    fi
+    if ((10#${ver1[i]} > 10#${ver2[i]})); then
+      return 1
+    fi
+    if ((10#${ver1[i]} < 10#${ver2[i]})); then
+      return 2
+    fi
+  done
+  return 0
+}
+
+# Display update prompt
+
+UPDATE_PROMPT="🎉🦋 A new version of Flutter For Web Build Script is availible!\nVisit https://github.com/FrankFlitton/Flutter-for-web-deploy-script to update."
+NO_UPDATE_PROMPT="👍 You are on the latest version of Flutter For Web Build Script. \n Beginning build now..."
+vercomp $CURRENT_VERSION $REMOTE_VERSION && echo $NO_UPDATE_PROMPT || echo $UPDATE_PROMPT
+
+# Begin to build the app for prod
 
 APP_DIR="$(
   cd -P "$(dirname "${BASH_SOURCE[0]}")"
